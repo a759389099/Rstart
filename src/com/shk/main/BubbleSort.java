427a7a26533ccb0487d5a740df4f5bcad36882e8
shk <hongkun.shen@newtouch.com>
2021-01-09 01:29:10 +0800

    冒泡排序

---
+++ src/com/shk/main/BubbleSort.java
@@ -0,0 +1,37 @@
+package com.shk.main;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] sort = { 1, 2, -100, 6, -55, 8, 4, 155, 0 };
+		int[] newSort = bubblesort(sort);
+		for (int i = 0; i < newSort.length; i++) {
+			System.out.println(newSort[i]);
+		}
+		
+	}
+/**
+ * 冒泡排序
+ * @param sort
+ * @return
+ */
+	private static int[] bubblesort(int[] sort) {
+		// 冒泡排序法
+
+		for (int i = 0; i < sort.length - 1; i++) {
+
+			for (int j = i; j < sort.length - 1; j++) {
+				if (sort[i] > sort[j + 1]) {
+					int k = sort[i];
+					sort[i] = sort[j + 1];
+					sort[j + 1] = k;
+				}
+			}
+
+		}
+
+		return sort;
+	}
+
+}
