2ddf437cdc9a0bb8eda270e92496250013cd182a
shk <hongkun.shen@newtouch.com>
2021-01-09 13:38:23 +0800

    添加注释

--- src/com/shk/main/BubbleSort.java
+++ src/com/shk/main/BubbleSort.java
@@ -1,10 +1,12 @@
 package com.shk.main;
 
 public class BubbleSort {
-
+/**
+ * 主方法
+ * @param args
+ */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] sort = { 1, 2, -100, 6, -55, 8, 4, 155, 0 };
+		int[] sort = { -100, 2, 777, 6, -55, 8, 4, 155, 0 };
 		int[] newSort = bubblesort(sort);
 		for (int i = 0; i < newSort.length; i++) {
 			System.out.println(newSort[i]);
@@ -13,7 +15,7 @@ public class BubbleSort {
 	}
 /**
  * 冒泡排序
- * @param sort
+ * @param sort 
  * @return
  */
 	private static int[] bubblesort(int[] sort) {
