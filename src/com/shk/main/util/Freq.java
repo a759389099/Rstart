040e32348bcb09c3dd5d894c42b2e47bf405bc2b
zed <401838601@qq.com>
2021-07-03 17:33:34 +0800

    leetcode三个数之和问题

--- src/com/shk/main/util/Freq.java
+++ src/com/shk/main/util/Freq.java
@@ -1,22 +1,17 @@
 package com.shk.main.util;
+
 import java.util.*;
 
 /*
-方法一：按照出现频率排序
-        题目要求将给定的字符串按照字符出现的频率降序排序，因此需要首先遍历字符串，统计每个字符出现的频率，然后每次得到频率最高的字符，生成排序后的字符串。
-
-        可以使用哈希表记录每个字符出现的频率，将字符去重后存入列表，再将列表中的字符按照频率降序排序。
-
-        生成排序后的字符串时，遍历列表中的每个字符，则遍历顺序为字符按照频率递减的顺序。对于每个字符，将该字符按照出现频率拼接到排序后的字符串。例如，遍历到字符 cc，该字符在字符串中出现了 \textit{freq}freq 次，则将 \textit{freq}freq 个字符 cc 拼接到排序后的字符串。
-        也可以使用优先队列或大根堆存储字符，读者可以自行尝试。*/
+给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+*/
 public class Freq {
 
     public static void main(String[] args) {
-        System.out.println(sortStr("sortStr"));
+        System.out.println(sortStr("tree"));
     }
 
     public static String sortStr(String originStr) {
-
 
         HashMap<Character, Integer> map = new HashMap<>();
         Integer length = originStr.length();
@@ -26,12 +21,33 @@ public class Freq {
             map.put(c, frequent);
         }
         List<Character> list = new ArrayList<>(map.keySet());
-
-
-        list.sort((o1, o2) -> o2 - o1);
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
         StringBuilder sb = new StringBuilder();
-        list.forEach(sb::append);
+        list.forEach(character -> {
+            for (int i = 0; i < map.get(character); i++)
+                sb.append(character);
+        });
         return sb.toString();
     }
+
+
+    /**
+     * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        int length = nums.length;
+
+        for (int i = 0; i < length - 2; i++) {
+            int j = i + 1;
+            int k = length - 1;
+
+
+        }
+
+        return null;
+    }
+
+        return null;
+}
 
 }
