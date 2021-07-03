c8f23a1c5027eefbba80ce9d6ba2e2e9827d679e
zed <401838601@qq.com>
2021-07-03 19:01:35 +0800

    leetcode三个数之和问题

--- src/com/shk/main/util/Freq.java
+++ src/com/shk/main/util/Freq.java
@@ -9,12 +9,15 @@ public class Freq {
 
     public static void main(String[] args) {
         System.out.println(sortStr("tree"));
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        threeSum(nums).forEach(System.out::println);
+
     }
 
     public static String sortStr(String originStr) {
 
         HashMap<Character, Integer> map = new HashMap<>();
-        Integer length = originStr.length();
+        int length = originStr.length();
         for (int i = 0; i < length; i++) {
             char c = originStr.charAt(i);
             Integer frequent = map.getOrDefault(c, 0) + 1;
@@ -34,20 +37,58 @@ public class Freq {
     /**
      * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
      */
-    public List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        HashSet<List<Integer>> resultset = new HashSet<>();
+        List<List<Integer>> resultList =new ArrayList<>(resultset);
+
         int length = nums.length;
+        if (length < 3) {
+            // 数组长度不能小于3
+            return resultList;
+        }
+        // 先冒泡排序
+        for (int i = 0; i < nums.length - 1; i++) {
 
-        for (int i = 0; i < length - 2; i++) {
-            int j = i + 1;
-            int k = length - 1;
-
+            for (int j = i; j < nums.length - 1; j++) {
+                if (nums[i] > nums[j + 1]) {
+                    int k = nums[i];
+                    nums[i] = nums[j + 1];
+                    nums[j + 1] = k;
+                }
+            }
 
         }
 
-        return null;
+
+        // 第一个指针
+        for (int i = 0; i < length - 2; i++) {
+            // 左指针
+            int j = i + 1;
+            // 右指针
+            int k = length - 1;
+            while (j < k) {
+                List<Integer> integerList = new ArrayList<>();
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    // 存到集合里
+                    integerList.add(nums[i]);
+                    integerList.add(nums[j]);
+                    integerList.add(nums[k]);
+                    resultset.add(integerList);
+                    // 左移,右移皆可
+                    j++;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    // 小于0 左指针右移
+                    j++;
+                } else {
+                    // 大于0 右指针左移
+                    // nums[i] +nums[j] +nums[k]>0
+                    k--;
+                }
+            }
+        }
+        resultList =new ArrayList<>(resultset);
+
+        return resultList;
     }
-
-        return null;
-}
-
 }
